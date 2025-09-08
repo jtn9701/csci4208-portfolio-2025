bac565542ccbccf517f64123883b1ff71567c02a
jtn9701 <jtnguy01@gmail.com>
2025-09-08 18:16:34 -0500

    Start Lab4 and finish part 3-2

---
+++ Lab4/hilo-js-dom-master/scripts/game.js
@@ -0,0 +1,43 @@
+/* Get HTML Elements as JS objects */
+const button = document.getElementById("guess-button");
+const number = document.getElementById("guess-text");
+
+/* Add Event Listener to button with callback function*/
+button.addEventListener("click", guessNumber);
+
+/* Callback function for event: Button click */
+function guessNumber() {
+const guess = number.value;
+console.log(guess);
+}
+
+/* Hi-Lo Game Data*/
+const passcode = Math.floor( Math.random() * 1000 );
+let tries = 10;
+
+/* Callback function for event: Button click */
+function guessNumber() {
+    const guess = number.value;
+    tries--;
+    attemptsView.innerHTML = `Number of attempts left: ${tries}`;
+    if ( guess == passcode){
+        document.body.innerHTML = `<h1>You win!</h1> <p>Got it in ${10-tries} attempts</p>`;    }
+    else if (tries < 0){
+        document.body.innerHTML = `<h1>You lose!</h1> <p>The passcode was ${passcode}</p>`;    }
+    else{
+        giveClue(guess)
+    }
+}
+
+/*Give Clue */
+function giveClue(guess){
+    if (guess > passcode){
+        cluesView.innerHTML += `<li>${guess} is too High!</li>`;
+    }
+    else{
+        cluesView.innerHTML += `<li>${guess} is too Low!</li>`;
+    }
+}
+
+const attemptsView = document.getElementById("attempts");
+const cluesView = document.getElementById("clues");
