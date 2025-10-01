73ea18cf0655cd11aab0d9648fa29b76d00b6b01
jtn9701 <jtnguy01@gmail.com>
2025-10-01 17:27:31 -0500

    Complete goal 1-5

--- Labs/Lab6/quiz-game-master/scripts/app.js
+++ Labs/Lab6/quiz-game-master/scripts/app.js
@@ -2,7 +2,12 @@ import * as http from './http.js' //Import http functions
 import * as view from './view.js'; //Import view functions
 
 const GET_TRIVIA = `https://opentdb.com/api.php?amount=1&difficulty=easy`; //Trivia GET endpoint
-const state = {}; //Game start
+const state = {
+    score: 0,
+    timer: 20,
+    intervalId: null,
+    trivia: null
+}; //Game start
 
 const playGame = async () => { //PLAY function
     const json = await http.sendGETRequest(GET_TRIVIA); //GET Request for trivia data
@@ -11,6 +16,20 @@ const playGame = async () => { //PLAY function
 }
 
 window.start = async () => { //START function
-    playGame(); //call play function
+    createGame(); 
 }
+
 window.addEventListener('load', start); 
+
+const countdown = () => { //COUNTDOWN function
+    if (state.timer){ //check if time remains
+        state.timer--; //decrement timer
+        view.PlayScene(state); //view render play scene
+    }
+}
+
+const createGame = () => { //CREATE function
+    state.timer = 20; //set timer
+    state.intervalId = setInterval(countdown, 1000); //set interval id
+    playGame(); //call PLAY function
+}
--- Labs/Lab6/quiz-game-master/scripts/view.js
+++ Labs/Lab6/quiz-game-master/scripts/view.js
@@ -1,10 +1,12 @@
 import Question from './components/Question.js'; //Import Question function
+import HUD from './components/HUD.js'; //Import HUD function
 
 const renderDOM = (html) => document.getElementById('view').innerHTML = html; //Set HTML in view
 
 export const PlayScene = (props) => { //Function for HTML view
-    const {trivia} = props; //Destructure properties
+    const {timer, score, trivia} = props; //Destructure properties
     renderDOM( //render the Scene's HTML to DOM
-        `${Question(trivia)}`
+        `${HUD(timer, score)}
+        ${Question(trivia)}`
     )
 }
