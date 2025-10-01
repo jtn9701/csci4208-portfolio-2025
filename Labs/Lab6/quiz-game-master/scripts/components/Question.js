243dd6dc74a26582db42549c1c3f172e9a88c6fd
jtn9701 <jtnguy01@gmail.com>
2025-10-01 17:22:59 -0500

    Complete Goal 1-4

--- Labs/Lab6/quiz-game-master/scripts/components/Question.js
+++ Labs/Lab6/quiz-game-master/scripts/components/Question.js
@@ -1,9 +1,12 @@
+import Options from './Option.js' //Import Options function
+
 const Question = (trivia) => ( //Function for HTML component
     `<h3>
         <div>Category - ${trivia.category}</div>
         <div>Difficulty - ${trivia.difficulty}</div>
     </h3>
     <h4>Question:</h4>
-    <p>${trivia.question}</p>`
+    <p>${trivia.question}</p>
+    ${Options(trivia)}`
 ) //Return HTML text
 export default Question;
